32549d6507e727098b265dae66e9115d5b45dd4d
alexandar89934 <aleksandar.radovanovic2712@gmail.com>
2022-10-09 14:59:15 +0200

    initial commit

---
+++ src/books.js
@@ -0,0 +1,21 @@
+//setup vars
+export const data = [
+  {
+    id: 1,
+    author: "Reyna Grande",
+    title: "A distance between us: Memoir",
+    img: "https://images.bwbcovers.com/145/9781451661781.jpg",
+  },
+  {
+    id: 2,
+    author: "Sonia Nazario",
+    title: "Enrique's Journey",
+    img: "https://images.bwbcovers.com/038/Enrique-s-Journey-The-Young-Adult-Adaptation-Nazario-Sonia-9780385743280.jpg",
+  },
+  {
+    id: 3,
+    author: "Colleen Hoover",
+    title: "Reminders of Him",
+    img: "https://images.bwbcovers.com/154/9781542025607.jpg",
+  },
+];
